281d0838822f3fd91bc8afd04ab8e8b860fa2e7e
pabloor29 <pab.ortg@gmail.com>
2026-06-20 11:25:32 +0200

    feat:

--- src/middleware.ts
+++ src/middleware.ts
@@ -120,6 +120,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|fonts|.*\\.(?:svg|png|jpg|jpeg|gif|webp|mp4|webm|mov|m4v|ogg|mp3|wav)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|fonts|.*\\.(?:svg|png|jpg|jpeg|gif|webp|mp4|webm|mov|m4v|ogg|mp3|wav)$).*)',
   ],
 }
